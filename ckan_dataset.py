93363ff2214ca8a5d02c045fc9806bb65f4dbd8c
Aitor Magán <amagan@conwet.com>
2015-10-16 14:43:49 +0200

    Improve the returned errors

--- ckan_dataset.py
+++ ckan_dataset.py
@@ -28,31 +28,55 @@ from urlparse import urlparse, urljoin
 
 class CKANDataset(Plugin):
 
+    def get_request(self, *args, **kwargs):
+
+        try:
+            return requests.get(*args, **kwargs)
+        except requests.ConnectionError:
+            raise PermissionDenied('Invalid resource: The CKAN server is not responding')
+
     def check_user_is_owner(self, provider, url):
 
+        # Get CKAN server URL
         parsed_url = urlparse(url)
         ckan_server = parsed_url.scheme + '://' + parsed_url.netloc
 
-        # Extract dataset id
+        # Extract dataset ID
         # Element 0 is empty
-        dataset_id = parsed_url.path.split('/')[2]
+        splitted_path = parsed_url.path.split('/')
+
+        # Create headers for the requests
+        headers = {'X-Auth-token': provider.userprofile.access_token}
+
+        if len(splitted_path) < 3:
+            raise PermissionDenied('Invalid resource: The provided URL does not point to a valid CKAN dataset')
+
+        if splitted_path[1] not in ('dataset', 'package'):
+            raise PermissionDenied('Invalid resource: The provided URL does not point to a valid CKAN dataset')
+
+        dataset_id = splitted_path[2]
 
         # Get dataset metainfo
         meta_url = urljoin(ckan_server, 'api/action/dataset_show?id=' + dataset_id)
-        meta_info_res = requests.get(meta_url, headers={'X-Auth-token': provider.userprofile.access_token})
+        meta_info_res = self.get_request(meta_url, headers=headers)
 
         if meta_info_res.status_code != 200:
-            raise PermissionDenied('Invalid resource: The user is not authorized to access the dataset')
+            error_msg = 'The dataset provided dataset does not exist' if meta_info_res.status_code == 404 else \
+                        'The user is not authorized to access the dataset'
+            raise PermissionDenied('Invalid resource: %s' % error_msg)
 
         meta_info = meta_info_res.json()
         user_id = meta_info['result']['creator_user_id']
 
         # Get user info
         user_url = urljoin(ckan_server, 'api/action/user_show?id=' + user_id)
-        user_info_res = requests.get(user_url, headers={'X-Auth-token': provider.userprofile.access_token})
+        user_info_res = self.get_request(user_url, headers=headers)
 
         if user_info_res.status_code != 200:
-            raise PermissionDenied('Invalid resource: The user is not authorized to access the dataset')
+            # When the current user cannot access the profile of the owner of dataset,
+            # it is assumed that the user is not the owner of the dataset since any user
+            # can access its own profile.
+            raise PermissionDenied('Invalid resource: The user is not the owner of the dataset')
 
         user_info = user_info_res.json()
 
