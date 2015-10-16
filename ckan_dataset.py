870faf1336d1d6b422898b347386adbb06641a42
Aitor Magán <amagan@conwet.com>
2015-10-16 13:41:46 +0200

    Add license

--- ckan_dataset.py
+++ ckan_dataset.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 CoNWeT Lab., Universidad Politécnica de Madrid
+
+# This file is part of WStore CKAN plugin.
+
+# WStore CKAN plugin is free software: you can redistribute it
+# and/or modify it under the terms of the European Union Public
+# Licence (EUPL) as published by the European Commission, either
+# version 1.1 of the License, or (at your option) any later
+# version.
+
+# WStore CKAN plugin is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the European Union Public Licence for more details.
+
+# You should have received a copy of the European Union Public Licence
+# along with WStore CKAN plugin.
+# If not, see <https://joinup.ec.europa.eu/software/page/eupl/licence-eupl>.
+
 import requests
 
 from django.core.exceptions import PermissionDenied
